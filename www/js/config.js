d4c9a86bac97097ef7f1a30d57433a50b9411ae7
srikutty19 <sri9132@gmail.com>
2019-03-21 14:17:21 +0530

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://rapidtest.ga/restomulti/mobileapp/api",	
+	'DialogDefaultTitle':"KMRS",	
+	'pushNotificationSenderid':"",	
+	'facebookAppId':"", 
+	'APIHasKey':"bc0366d2495f330329ba4004323b0b79"
+};
