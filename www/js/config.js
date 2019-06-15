4960e71074219ad321bbdfd1371167fee0337659
srikutty19 <sri9132@gmail.com>
2019-06-15 20:02:51 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
-	'ApiUrl':"http://rapidtest.ga/restomulti/mobileapp/api",	
+	'ApiUrl':"http://athavtech.com/AppProjects/foodable/mobileapp/api",	
 	'DialogDefaultTitle':"Foodable Mobile App",	
 	'pushNotificationSenderid':"AIzaSyCU4x1U-7Am-AbpXzoqcSJzjsUR3CeBGjQ",	
 	'facebookAppId':"1079052755611433", 
-	'APIHasKey':"007b9d5fab40cd27208c66fd1751e396"
+	'APIHasKey':"bc1d4aef35e399c9c4b96ae7ea0b5970"
 };
