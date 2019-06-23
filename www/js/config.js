4ae0adfd4af30965a89cb262df622abb38d97228
srikutty19 <sri9132@gmail.com>
2019-06-23 08:45:26 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
-	'ApiUrl':"http://athavtech.com/AppProjects/foodable/mobileapp/api",	
+	'ApiUrl':"http://foodable.athavtech.com/mobileapp/api",	
 	'DialogDefaultTitle':"Foodable Mobile App",	
 	'pushNotificationSenderid':"AIzaSyCU4x1U-7Am-AbpXzoqcSJzjsUR3CeBGjQ",	
 	'facebookAppId':"1079052755611433", 
-	'APIHasKey':"bc1d4aef35e399c9c4b96ae7ea0b5970"
+	'APIHasKey':"e3e279a6a040fe77d5d602f828e2f9c2"
 };
